91aa3dfd53f3498793057bbca8593ae7e922fdd4
Gustavo Dominguez <gfdominguez119@gmail.com>
2026-03-18 11:40:28 -0300

    Actualizar exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,6 @@ export type { FacturaElectronica } from './sifen/types/factura';
 export { SifenAPI, type SIFENConfig } from './client/sifen-client';
 export { type XMLGenerator } from './xml-gen/xml-generator';
 export { type QRGenerator } from './qr/qr-generator';
-export { type SIFENResponse } from './sifen/types/response-clean';
 export * from './sifen/types/common';
 export { type CertificateData, CertificateManager } from './certificate/certificate-manager';
 
