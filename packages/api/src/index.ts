41625384a38aecf4f8aee6db94765619bb129c4e
Gustavo Dominguez <gfdominguez119@gmail.com>
2026-04-01 11:39:52 -0300

    Reparar exports base

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,10 @@
-export type { FacturaElectronica } from './sifen/types/old_do_not_use/factura';
 export { SifenAPI, type SIFENConfig } from './client/sifen-client';
-export { type XMLGenerator } from './xml-gen/old_generator';
+export { generateFacturaElectronicaXML } from './xml-gen';
 export { attachQRToSignedXML } from './qr/qr-generator';
-export * from './sifen/types/old_do_not_use';
+export * from './sifen/types';
 export { type CertificateData, CertificateManager } from './certificate/certificate-manager';
 
-export { XMLGen } from './xml-gen/old_generator';
+export * from './xml-gen/factura-electronica';
 export * from './xml-gen/mapper';
 
 export { XMLSigner } from './xml-sign/xml-signer';
