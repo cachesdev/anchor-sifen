f7e86fcb0bf61f9c6efa6fc909533e2ca669f8a7
Gustavo Dominguez <gfdominguez119@gmail.com>
2026-04-11 15:31:09 -0300

    Checkear que los campos calculados esten correctos, eliminar todas las reglas de validacion

--- packages/api/src/xml-gen/derive/big.ts
+++ packages/api/src/xml-gen/derive/big.ts
@@ -4,17 +4,10 @@ export function num(value: number): Big {
   return new Big(value);
 }
 
-export const SCALE_GENERAL = 8;
-const SCALE_REDONDEO = 4;
-
 export const ZERO = num(0);
 export const ONE = num(1);
 export const HUNDRED = num(100);
 
 export function bigOrZero(value: Big | undefined): Big {
   return value ?? ZERO;
-}
-
-export function quantizeRedondeo(value: Big): Big {
-  return value.round(SCALE_REDONDEO, Big.roundHalfUp);
 }
