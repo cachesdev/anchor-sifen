21383b90ab937c5392b3963db4f93c3ae678225b
Gustavo Dominguez <gfdominguez119@gmail.com>
2026-04-26 17:08:55 -0300

    Simplificar schemas dramaticamente solo checkeando enums

--- packages/api/src/xml-gen/schema/de.ts
+++ packages/api/src/xml-gen/schema/de.ts
@@ -1,18 +1,6 @@
 import * as v from 'valibot';
-import type { OperacionDE_FE_Input, Timbrado_FE_Input } from '../../sifen/types';
 import { tipoEmision } from '../../sifen/types/enums';
 
-export const operacionDESchema = v.object({
-  tipoEmision: v.enum(tipoEmision),
-  informacionEmisor: v.optional(v.string()),
-  informacionFisco: v.optional(v.string())
-}) satisfies v.GenericSchema<OperacionDE_FE_Input>;
-
-export const timbradoFESchema = v.object({
-  numeroTimbrado: v.number(),
-  establecimiento: v.number(),
-  puntoExpedicion: v.number(),
-  numeroDocumento: v.number(),
-  serieNumero: v.optional(v.string()),
-  fechaInicioVigencia: v.date()
-}) satisfies v.GenericSchema<Timbrado_FE_Input>;
+export const operacionDEEnumSchema = v.looseObject({
+  tipoEmision: v.enum(tipoEmision)
+});
--- packages/api/src/xml-gen/schema/f.ts
+++ packages/api/src/xml-gen/schema/f.ts
@@ -1,6 +1,3 @@
 import * as v from 'valibot';
-import type { SubtotalesTotales_FE_Input } from '../../sifen/types';
 
-export const subtotalesTotalesSchema = v.object({
-  comisionOperacion: v.optional(v.number())
-}) satisfies v.GenericSchema<SubtotalesTotales_FE_Input>;
+export const subtotalesTotalesEnumSchema = v.looseObject({});
